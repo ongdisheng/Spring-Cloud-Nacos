7bc7b10234cca0e9db8ac965111a36c9fd1381ec
disheng <ongdisheng@gmail.com>
2024-07-25 10:40:10 +0800

    Added nacos service discovery code implementation

--- cart-service/src/main/java/com/example/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/example/service/impl/CartServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.example.mapper.CartMapper;
 import com.example.pojo.CartItem;
 import com.example.pojo.Item;
 import com.example.service.CartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +28,9 @@ public class CartServiceImpl implements CartService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Override
     public List<CartItem> getCartItemsByUserID(Integer userID) {
         List<CartItem> cartItems = cartMapper.getCartItemsByUserID(userID);
@@ -33,19 +39,44 @@ public class CartServiceImpl implements CartService {
         List<Integer> cartItemIDs = cartItems.stream().map(cartItem -> cartItem.getItemID()).collect(Collectors.toList());
         String ids = cartItemIDs.stream().map(String::valueOf).collect(Collectors.joining(","));
 
+//        // A. without nacos
+//        // make HTTP request to item service for getting item price info
+//        String resourceUrl = "http://localhost:8080/items";
+//        List<Item> items = restTemplate.exchange(
+//                resourceUrl + "/" + ids,
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<Item>>() {
+//                }
+//        ).getBody();
+
+        // B. with nacos (resolve hardcoding issue)
+        // retrieve item service provider instances
+        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+
+        // simple load balancing implementation
+        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+
         // make HTTP request to item service for getting item price info
-        String resourceUrl = "http://localhost:8080/items";
-        List<Item> items = restTemplate.exchange(
-                resourceUrl + "/" + ids,
+        ResponseEntity<List<Item>> response = restTemplate.exchange(
+                instance.getUri() + "/items/" + ids,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<Item>>() {
                 }
-        ).getBody();
-        for (int i = 0; i < cartItems.size(); i++) {
-            cartItems.get(i).setItemPrice(items.get(i).getPrice());
-        }
+        );
 
-        return cartItems;
+        // handle response
+        List<Item> items = null;
+        if (response.getStatusCode().is2xxSuccessful()) {
+            items = response.getBody();
+            for (int i = 0; i < cartItems.size(); i++) {
+                cartItems.get(i).setItemPrice(items.get(i).getPrice());
+            }
+
+            return cartItems;
+        } else {
+            return null;
+        }
     }
 }
